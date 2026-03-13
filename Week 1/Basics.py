53304275e3b2367a3a2454d1ffc80cb17c5e4b0a
ajosan123 <ajosan@eastsideprep.org>
2026-03-12 20:23:30 -0700

    Week 1: Add graph libraries to requirements and create graph demos

--- Week 1/Basics.py
+++ Week 1/Basics.py
@@ -1,4 +1,28 @@
 import networkx as nx
-import nunpy as np
-import matplotlib as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Erdős-Rényi Graph (Random Graph)
+def erdos_renyi_demo():
+    n = 20  # Number of nodes
+    p = 0.2  # Probability of edge creation
+    G = nx.erdos_renyi_graph(n, p)
+    plt.figure(figsize=(6, 4))
+    nx.draw(G, with_labels=True, node_color='skyblue', edge_color='gray')
+    plt.title("Erdős-Rényi Graph (n=20, p=0.2)")
+    plt.show()
+
+# Barabási-Albert Graph (Scale-Free Graph)
+def barabasi_albert_demo():
+    n = 20  # Number of nodes
+    m = 2   # Number of edges to attach from a new node to existing nodes
+    G = nx.barabasi_albert_graph(n, m)
+    plt.figure(figsize=(6, 4))
+    nx.draw(G, with_labels=True, node_color='lightgreen', edge_color='gray')
+    plt.title("Barabási-Albert Graph (n=20, m=2)")
+    plt.show()
+
+print("Erdős-Rényi Graph Demo:")
+erdos_renyi_demo()
+print("Barabási-Albert Graph Demo:")
+barabasi_albert_demo()
